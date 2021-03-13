112ebcca68085e35dcad1fbdacba9a8888834c1a
Fispit <sdelcos@gmail.com>
2021-03-13 17:55:31 -0600

    Code compacting and cleanup

--- stonks.vb
+++ stonks.vb
@@ -5,7 +5,6 @@ Sub calculate()
 
 For s = 1 To Sheets.Count
 Dim ws As Worksheet
-
 Set ws = Worksheets(s)
 
 ws.Cells(1, 10) = "Ticker"
@@ -19,14 +18,12 @@ ws.Cells(2, 15) = "Greatest % Increase"
 ws.Cells(3, 15) = "Greatest % Decrease"
 ws.Cells(4, 15) = "Stock Volume"
 
-greatestincrease = 0#
-greatestdecrease = 0#
+greatestincrease = 0
+greatestdecrease = 0
 maxvolume = 0
-
-
-usedrows = Rows.Count
+usedrows = ws.Rows.Count
 outputrow = 2
-outputrow2 = 2
+
 For i = 2 To usedrows - 1
     ticker = ws.Cells(i, 1)
     nexttick = ws.Cells(i + 1, 1)
@@ -45,16 +42,12 @@ For i = 2 To usedrows - 1
             ws.Cells(outputrow, 12) = (closevalue - initialopen) / initialopen
             End If
             ws.Cells(outputrow, 13) = stvolume
-            
     ElseIf i = 2 Then
         initialopen = ws.Cells(i, 3)
         stvolume = ws.Cells(i, 7)
     Else
-
         If ticker = nexttick Then
             stvolume = stvolume + ws.Cells(i, 7)
-
-        
         Else
             openvalue = ws.Cells(i, 3)
             high = ws.Cells(i, 4)
@@ -64,7 +57,7 @@ For i = 2 To usedrows - 1
             ws.Cells(outputrow, 10) = ticker
             ws.Cells(outputrow, 11) = closevalue - initialopen
             If initialopen = 0 Then
-            
+        
             Else
             percentagechange = ((closevalue - initialopen) / initialopen) * 100
             ws.Cells(outputrow, 12) = percentagechange
@@ -74,12 +67,8 @@ For i = 2 To usedrows - 1
                     ws.Cells(outputrow, 12).Interior.ColorIndex = 3
                 Else
                     ws.Cells(outputrow, 12).Interior.ColorIndex = 6
-
                 End If
-
-            
             End If
-            
             ws.Cells(outputrow, 13) = stvolume
             initialopen = ws.Cells(i + 1, 3)
             If stvolume > maxvolume Then
@@ -87,45 +76,20 @@ For i = 2 To usedrows - 1
                 ws.Cells(4, 16) = ticker
                 ws.Cells(4, 17) = maxvolume
             End If
-
             If percentagechange > greatestincrease Then
                 greatestincrease = percentagechange
                 ws.Cells(2, 16) = ticker
                 ws.Cells(2, 17) = Str(greatestincrease) + " %"
             End If
-
             If percentagechange < greatestdecrease Then
                 greatestdecrease = percentagechange
                 ws.Cells(3, 16) = ticker
                 ws.Cells(3, 17) = Str(greatestdecrease) + " %"
-
             End If
-
-
-
-
-
-
             outputrow = outputrow + 1
             stvolume = ws.Cells(i + 1, 7)
-
-
-
-
-
-
-
-
         End If
-
-
-
-
     End If
-
-
-
-
 Next i
 Next s
 
