639ed0c7361e49711fc2168fe4640c636b0bf73a
Fispit <sdelcos@gmail.com>
2021-03-13 17:46:09 -0600

    Added maximum increase, decrease and volume to the script

--- stonks.vb
+++ stonks.vb
@@ -13,15 +13,18 @@ ws.Cells(1, 11) = "Amount Change"
 ws.Cells(1, 12) = "Percentage Change"
 ws.Cells(1, 13) = "Stock Volume"
 
-ws.Cells(1, 14) = "Ticker"
-ws.Cells(1, 15) = "Amount Change"
-ws.Cells(1, 16) = "Percentage Change"
-ws.Cells(1, 17) = "Stock Volume"
-ws.Cells(1, 18) = "Stock Volume"
+ws.Cells(1, 16) = "Ticker"
+ws.Cells(1, 17) = "Value"
+ws.Cells(2, 15) = "Greatest % Increase"
+ws.Cells(3, 15) = "Greatest % Decrease"
+ws.Cells(4, 15) = "Stock Volume"
+
+greatestincrease = 0#
+greatestdecrease = 0#
+maxvolume = 0
 
 
 usedrows = Rows.Count
-MsgBox (Str(usedrows))
 outputrow = 2
 outputrow2 = 2
 For i = 2 To usedrows - 1
@@ -63,7 +66,8 @@ For i = 2 To usedrows - 1
             If initialopen = 0 Then
             
             Else
-            ws.Cells(outputrow, 12) = ((closevalue - initialopen) / initialopen) * 100
+            percentagechange = ((closevalue - initialopen) / initialopen) * 100
+            ws.Cells(outputrow, 12) = percentagechange
                 If ws.Cells(outputrow, 12) > 0 Then
                     ws.Cells(outputrow, 12).Interior.ColorIndex = 4
                 ElseIf ws.Cells(outputrow, 12) < 0 Then
@@ -78,8 +82,36 @@ For i = 2 To usedrows - 1
             
             ws.Cells(outputrow, 13) = stvolume
             initialopen = ws.Cells(i + 1, 3)
+            If stvolume > maxvolume Then
+                maxvolume = stvolume
+                ws.Cells(4, 16) = ticker
+                ws.Cells(4, 17) = maxvolume
+            End If
+
+            If percentagechange > greatestincrease Then
+                greatestincrease = percentagechange
+                ws.Cells(2, 16) = ticker
+                ws.Cells(2, 17) = Str(greatestincrease) + " %"
+            End If
+
+            If percentagechange < greatestdecrease Then
+                greatestdecrease = percentagechange
+                ws.Cells(3, 16) = ticker
+                ws.Cells(3, 17) = Str(greatestdecrease) + " %"
+
+            End If
+
+
+
+
+
+
             outputrow = outputrow + 1
             stvolume = ws.Cells(i + 1, 7)
+
+
+
+
 
 
 
@@ -98,5 +130,6 @@ Next i
 Next s
 
 End Sub
+
 
 
